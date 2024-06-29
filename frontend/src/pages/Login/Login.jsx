ed2a0e103b85b960dc09329e33d6cc8536da8abd
m1ntc4ndyy <123210141@sv1.dut.udn.vn>
2024-06-29 16:33:52 +0700

    Login layout, show and hide password input

---
+++ frontend/src/pages/Login/Login.jsx
@@ -0,0 +1,46 @@
+import PasswordInput from "../../components/Input/PasswordInput.jsx"
+import NavBar from "../../components/NavBar/NavBar.jsx"
+import { Link } from "react-router-dom"
+import { useState } from "react"
+import { validateEmail } from "../../utils/helper.js"
+const Login = () => {
+  const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [error, setError] = useState(null);
+
+  // const handleLogin = async (e) => {
+  //   e.preventDefault();
+  //   if (!validateEmail(email)) {
+  //     setError("Invalid Email");
+  //     return;
+  //   }
+  // }
+
+  return (
+    <>
+     <NavBar /> 
+     <div className="flex item-center justify-center mt-28">
+      <div className="w-96 border rounded-lg bg-white px-7 py-10">
+        <form onSubmit={() => {handleLogin}}>
+          <h4 className="text-2xl mb-7 select-none">Login</h4>
+          <input type="text" placeholder="Email" className="input-box select-none"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+          <PasswordInput />
+          {/* value={password} onChange={(e)=>{setPassword(e.target.value)}} */}
+          {/* {error && <p className="text-red-500 text-xs pb-1 select-none">{error}</p>} */}
+
+          <button type="submit" className="btn-primary select-none">login</button>
+          <p className="text-sm text-center mt-4 select-none"> 
+            Not registered yet?{" "}
+            <Link to="/signup" className="text-blue-600 select-none">Create a Account</Link>
+          </p>
+        </form>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default Login
