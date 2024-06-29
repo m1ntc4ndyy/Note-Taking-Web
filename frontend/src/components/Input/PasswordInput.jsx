aa9ec7898dadcbf170e30be13534c6390da1ee34
m1ntc4ndyy <123210141@sv1.dut.udn.vn>
2024-06-29 23:41:38 +0700

    fixed login handling

--- frontend/src/components/Input/PasswordInput.jsx
+++ frontend/src/components/Input/PasswordInput.jsx
@@ -7,11 +7,13 @@ const PasswordInput = ({value, onChange, placeholder}) => {
   const toggleShowPassword = () => {
       let currentShowPassword = showPassword;
       setShowPassword(!currentShowPassword);
-  }
+    
+}
   return (
     <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3'>
         <input 
             value={value}
+            onChange={onChange}
             type={showPassword ? 'text' : 'password'} 
             placeholder={placeholder || 'Password'} 
             className='w-full bg-transparent text-sm py-3 mr-3 rounded outline-none select-none'
--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -5,31 +5,40 @@ import { useState } from "react"
 import { validateEmail } from "../../utils/helper.js"
 const Login = () => {
   const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [error, setError] = useState(null);
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-  // const handleLogin = async (e) => {
-  //   e.preventDefault();
-  //   if (!validateEmail(email)) {
-  //     setError("Invalid Email");
-  //     return;
-  //   }
-  // }
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    console.log(email);
+    if (!validateEmail(email)) {
+      setError("Invalid Email");
+      return;
+    }
+    if (!password) {
+      setError("Password is required");
+      return;
+    }
+    setError("");
+  }
 
   return (
     <>
      <NavBar /> 
      <div className="flex item-center justify-center mt-28">
       <div className="w-96 border rounded-lg bg-white px-7 py-10">
-        <form onSubmit={() => {handleLogin}}>
+        <form onSubmit={handleLogin}>
           <h4 className="text-2xl mb-7 select-none">Login</h4>
           <input type="text" placeholder="Email" className="input-box select-none"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           ></input>
-          <PasswordInput />
-          {/* value={password} onChange={(e)=>{setPassword(e.target.value)}} */}
-          {/* {error && <p className="text-red-500 text-xs pb-1 select-none">{error}</p>} */}
+          <PasswordInput 
+            value={password} 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+
+          {error && <p className="text-red-500 text-xs pb-1 select-none">{error}</p>}
 
           <button type="submit" className="btn-primary select-none">login</button>
           <p className="text-sm text-center mt-4 select-none"> 
